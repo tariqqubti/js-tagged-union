fb552d998d1596fc9066960e3b78cc59bed43beb
Tariq Qubti <tariq.qubti@gmail.com>
2020-07-22 20:19:45 +0300

    Redesign CheckForm back to take objects

--- src/Valid.js
+++ src/Valid.js
@@ -6,12 +6,16 @@ export const Fail = Make('Fail');
 export const Check = (x, predicate) => 
   predicate(x) ? Pass(x) : Fail(x);
 
-export const CheckForm = form => {
-  const passed = [];
-  for(const [val, validator] of form)
-    if(validator(val)) passed.push(val);
+export const CheckForm = (form, validators) => {
+  const keys = Object.keys(validators);
 
-  if(form.length === passed.length)
+  const passed = {};
+  for(const key of keys)
+    if(validators[key](form[key]))
+      passed[key] = form[key];
+
+  const passedKeys = Object.keys(passed);
+  if(passedKeys.length === keys.length)
     return Pass(passed);
   else
     return Fail(passed);
